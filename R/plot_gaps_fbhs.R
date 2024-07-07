4708cc5621751a00302d96ada42beaa22f00a7ec
olgaviedma <olga.viedma@uclm.es>
2024-07-07 08:52:06 +0200

    Merge branch 'master' of origin

--- R/plot_gaps_fbhs.R
+++ R/plot_gaps_fbhs.R
@@ -1,12 +1,13 @@
 #' Plots of tree profiles with gaps and fuel layers base height (fbh)
 #' @description This function plots gaps and fuel layers base height (fbh) in the vertical tree profile (VTP).
-#' @usage get_plots_gap_fbh (LAD_profiles,depth_metrics)
+#' @usage get_plots_gap_fbh (LAD_profiles,gap_cbh_metrics,min_height=1.5)
 #' @param LAD_profiles original tree Leaf Area Density (LAD) profile (output of [lad.profile()] function in the \emph{leafR} package.
 #' An object of the class text
-#' @param depth_metrics tree metrics with gaps (distances), fuel base heights and depths (output of [get_depths()] function).
-#' An object of the class text
+#' @param gap_cbh_metrics data frame with gaps (distances) and fuel base heights (output of [get_gaps_fbhs()] function).
+#' An object of the class text.
+#' @param min_height Numeric value for the actual minimum base height (in meters).
 #' @return A plot drawing by lines the height of gaps and fuel layers bases in tiff format.
-#' @author Olga Viedma, Carlos Silva and JM Moreno
+#' @author Olga Viedma, Carlos Silva, JM Moreno and A.T. Hudak
 #'
 #' @examples
 #' library(ggplot2)
@@ -17,13 +18,13 @@
 #' header = TRUE)
 #' LAD_profiles$treeID <- factor(LAD_profiles$treeID)
 #'
-#' # Before running this example, make sure to run get_depths().
+#' # Before running this example, make sure to run get_gaps_fbhs().
 #' if (interactive()) {
-#' depth_metrics <- get_depths()
-#' LadderFuelsR::depth_metrics$treeID <- factor(LadderFuelsR::depth_metrics$treeID)
+#' gap_cbh_metrics <- get_gaps_fbhs()
+#' LadderFuelsR::gap_cbh_metrics$treeID <- factor(LadderFuelsR::gap_cbh_metrics$treeID)
 #'
 #' # Generate plots for gaps and fbhs
-#' plots_gaps_fbhs <- get_plots_gap_fbh(LAD_profiles, depth_metrics)
+#' plots_gaps_fbhs <- get_plots_gap_fbh(LAD_profiles, gap_cbh_metrics, min_height=1.5)
 #' }
 #' @importFrom dplyr select_if group_by summarise summarize mutate arrange rename rename_with filter slice slice_tail ungroup distinct
 #' across matches row_number all_of vars
@@ -32,17 +33,42 @@
 #' @importFrom stats ave dist lm na.omit predict quantile setNames smooth.spline
 #' @importFrom utils tail
 #' @importFrom tidyselect starts_with everything one_of
-#' @importFrom stringr str_extract str_match str_detect
+#' @importFrom stringr str_extract str_match str_detect str_remove_all
 #' @importFrom tibble tibble
 #' @importFrom tidyr pivot_longer fill
 #' @importFrom gdata startsWith
 #' @importFrom ggplot2 aes geom_line geom_path geom_point geom_polygon geom_text geom_vline ggtitle coord_flip theme_bw
 #' theme element_text xlab ylab ggplot
-#' @seealso \code{\link{get_depths}}
+#' @seealso \code{\link{get_gaps_fbhs}}
 #' @export
-get_plots_gap_fbh <- function (LAD_profiles,depth_metrics) {
+get_plots_gap_fbh <- function (LAD_profiles,gap_cbh_metrics,min_height=1.5) {
 
   df_orig<-LAD_profiles
+
+
+  if(min_height==0){
+    min_height <-0.5
+
+    # Ensure the column starts with a negative value
+    if (df_orig$height[1] < min_height) {
+      # Calculate the shift value
+      shift_value <- abs(df_orig$height[1])
+
+      # Adjust the column to start from 0
+      df_orig$height <- df_orig$height + shift_value
+    }
+
+
+    # Ensure the column starts with a negative value
+    if (df_orig$height[1] > min_height) {
+      # Calculate the shift value
+      shift_value1 <- abs(df_orig$height[1])
+
+      # Adjust the column to start from 0
+      df_orig$height <- df_orig$height - shift_value1
+    }
+  }
+
 
   df_orig$treeID<-factor(df_orig$treeID)
   treeID<-factor(df_orig$treeID)
@@ -50,7 +76,7 @@ get_plots_gap_fbh <- function (LAD_profiles,depth_metrics) {
   trees_name1a<- as.character(df_orig$treeID)
   trees_name3<- factor(unique(trees_name1a))
 
-  df_metrics<- depth_metrics
+  df_metrics<- gap_cbh_metrics
 
 
   plot_list <- list()
--- R/renamed_df.R
+++ R/renamed_df.R
@@ -12,86 +12,89 @@
 #' @importFrom stringr str_extract str_match str_detect
 #' @export
 get_renamed_df <- function(df) {
-
+  
   df1 <- df %>%
-    dplyr::select_if(~!all(is.na(.) | . == 0))
-
+    dplyr::select_if(~!all(is.na(.)))
+  
   # Get indices of columns containing "hcbh_hdptf"
   Hcbh_Hdptf_indices <- grep("Hcbh[0-9]_Hdptf[0-9]", names(df1), value = FALSE)
-
+  
   # Create two dataframes
   df1_Hcbh_Hdptf <- df1[, Hcbh_Hdptf_indices, drop = FALSE]
   df1_no_Hcbh_Hdptf <- df1[, -Hcbh_Hdptf_indices, drop = FALSE]
-
+  
   # Get column names
   col_names <- names(df1_no_Hcbh_Hdptf)
-
+  
   # Extract prefixes and suffixes
   col_parts <- str_match(col_names, "(\\D+)(\\d*)")
   prefixes <- col_parts[, 2]
   suffixes <- col_parts[, 3]
-
+  
   # Find which column names have numeric suffix
   has_numeric_suffix <- suffixes != ""
-
+  
   # Define a function to rename the columns with numeric suffixes
   rename_cols <- function(prefix) {
     # Find column names with the prefix
     cols <- col_names[prefixes == prefix & has_numeric_suffix]
-
+    
     # Extract the numeric suffixes and convert to integers
     nums <- as.integer(str_extract(cols, "\\d+$"))
-
+    
     # Generate the new names
     new_names <- paste0(prefix, seq_along(nums))
-
+    
     # Return a named vector of new names
     setNames(new_names, cols)
   }
-
+  
   # Apply the function to each prefix and unlist the results
   new_names <- unlist(lapply(unique(prefixes[has_numeric_suffix]), rename_cols))
-
+  
   # Rename the columns in the dataframe
   names(df1_no_Hcbh_Hdptf) <- ifelse(names(df1_no_Hcbh_Hdptf) %in% names(new_names), new_names[names(df1_no_Hcbh_Hdptf)], names(df1_no_Hcbh_Hdptf))
-
+  
   #########################################3
   # Special cases with double prefix and suffix
   # Initialize count to 1
   count <- 1
-
+  
   # Create a character vector to hold new column names
   new_col_names <- character(length(names(df1_Hcbh_Hdptf)))
-
+  
   # Iterate over all column names
   for (i in seq_along(names(df1_Hcbh_Hdptf))) {
-
+    
     # Get the current column name
     col <- names(df1_Hcbh_Hdptf)[i]
-
+    
     # Split by underscore
     parts <- unlist(strsplit(col, "_"))
-
+    
     # Get prefixes (strip numeric suffix)
     prefix1 <- sub("[0-9]+$", "", parts[1])
     prefix2 <- sub("[0-9]+$", "", parts[2])
-
+    
     # Construct new column name using the counter
     new_col_name <- paste0(prefix1, count, "_", prefix2, count)
-
+    
     # Store the new column name
     new_col_names[i] <- new_col_name
-
+    
     # Increment counter
     count <- count + 1
   }
-
+  
   # Update the column names of the dataframe
   names(df1_Hcbh_Hdptf) <- new_col_names
-
-  merged_df<-data.frame(df1_no_Hcbh_Hdptf,df1_Hcbh_Hdptf)
-
-  #################################################
-
+  
+  merged_df <- data.frame(df1_no_Hcbh_Hdptf, df1_Hcbh_Hdptf)
+  
+  # Remove columns with only NA values
+  merged_df <- merged_df %>%
+    dplyr::select_if(~!(all(is.na(.))))
+  
   return(merged_df)
 }
+
